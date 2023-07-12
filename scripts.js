6820665b847fea7fcaed0099b301f6a115c6520b
Teoh Boon Ping <boonping@Teohs-MacBook-Air.local>
2023-07-13 00:54:59 +0800

    refactor: remove unused and unnecessary files

--- scripts.js
+++ scripts.js
@@ -1,118 +1,94 @@
-function fetchAllPokemon() {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(out => {
-            const pokemonResults = out.results;
-            const pokemonURLs = [];
-
-            pokemonResults.forEach(result => {
-                pokemonURLs.push(result.url)
-            });
-            fetchPokemonDetails(pokemonURLs)
-        });
+async function fetchPokemonURLs() {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        if (response.ok) {
+            const data = await response.json();
+            const pokemonData = data.results;
+            const pokemonURLs = pokemonData.map(result => result.url);
+            
+            return pokemonURLs
+        } else {
+            throw new Error("Error fetching Pokemon from PokeAPI");
+        }
+    } catch {
+        console.log(error)
+    }
 }
 
-function fetchPokemonDetails(urls) {
+async function fetchPokemonData() {
+    const pokemonObj = [];
+    try {
+        const urls = await fetchPokemonURLs();
+        const responses = await Promise.all(
+            urls.map(url => fetch(url))
+        );
 
-    const pokemonDetailsObj = [];
+        const pokemonData = await Promise.all(
+            responses.map(response => response.json())
+        );
 
-    const fetchPromises = urls.map(url =>
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(pokemonDetails => {
-            console.log(pokemonDetails)
-            pokemonDetailsObj.push({
-                id: pokemonDetails.id,
-                name: pokemonDetails.name,
-                sprites: pokemonDetails.sprites.other.home.front_default,
-                height: pokemonDetails.height,
-                weight: pokemonDetails.weight,
-                types: pokemonDetails.types
+        pokemonData.forEach(pokemon => {
+            pokemonObj.push({
+                id: pokemon.id,
+                name: pokemon.name,
+                sprites: pokemon.sprites.other.home.front_default, // we are using this as our main image
+                height: pokemon.height,
+                weight: pokemon.weight,
+                types: pokemon.types
             });
         })
-    );
 
-    Promise.all(fetchPromises)
-    .then(() => {
+        return pokemonObj
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function fetchAndPopulatePokemon() {
+    try {
+        const pokemonObj = await fetchPokemonData();
         const pokemonContainer = document.getElementById("pokemon-column");
 
-        const pokemonTypesMapper = {}
-
-
-        for (const pokemon of pokemonDetailsObj.sort((a,b) => a.id - b.id)) {
+        for (const pokemon of pokemonObj.sort((a,b) => a.id - b.id)) {
             const pokemonDiv = document.createElement("div");
-            pokemonDiv.className = "col-lg pokemon-container";
+            const pokemonWeight = convertWeightFromHectogramsToKilograms(pokemon.weight)
+            const pokemonHeight = convertHeightFromDecimetersToMeters(pokemon.height)
+            pokemonDiv.className = "col-lg-4 col-md-6 col-12";
             pokemonDiv.innerHTML = `
-            <div class="card h-100">
-                <div class="text-center"><img id="pokemon-img" src="${pokemon.sprites}"></div>
-                <div class="card-body">
-                <h5 class="card-title" id="pokemon-name">${pokemon.name}</h5>
-                <div class="d-flex flex-row justify-content-center">
-                    <div class="icon electric p-2"><img src="assets/pokemon-type-icons/electric.svg" /></div>
-                    <div class="icon fighting p-2"><img src="assets/pokemon-type-icons/fighting.svg" /></div>
+                <div class="card">
+                    <div class="text-center">
+                        <img src="${pokemon.sprites}" class="card-img-top sprites" alt="${pokemon.name}">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${pokemon.name}</h5>
+                        <div class="d-flex flex-row justify-content-center">
+                        ${pokemon.types
+                            .map(type => `<div class="icon ${type.type.name} p-2"><img src="assets/pokemon-type-icons/${type.type.name}.svg" /></div>`)
+                            .join('')}
+                        </div>
+                        <div class="d-flex flex-row justify-content-center">
+                            <div class="scale"><i class="fa-solid fa-weight-scale"></i> ${pokemonWeight} kg </div>
+                            <div class="scale"><i class="fa-solid fa-ruler-vertical"></i> ${pokemonHeight} m </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="d-flex flex-row justify-content-center">
-                    <div class="scale"><i class="bi bi-rulers"></i> ${pokemon.weight} kg </div>
-                    <div class="scale"><i class="bi bi-rulers"></i> ${pokemon.height} cm </div>
-                </div>
-                </div>
-                <a href="#" class="btn btn-view" style="background-color: #F2D94E;">More Details</a>
-            </div>
             `;
             pokemonContainer.appendChild(pokemonDiv);
         }
-    })
-    .catch(error => {
-        console.log("Error fetching Pokemon details:", error);
-    });
+    } catch {
+        console.log(error)
+    }
 }
 
-function displayPokemon(pokemonDetails) {
-    // const pokemonResults = pokemonDetails;
-    // const pokemonContainer = document.getElementById("pokemon-column");
-    // console.log(pokemonDetails)
-    // Using the forEach() method
-    // pokemonDetails.forEach(pokemon => {
-    //     console.log(pokemon.name);
-    //     console.log(pokemon.sprites);
-    //     console.log(pokemon.height);
-    //     console.log(pokemon.weight);
-    // });
-  
-
-    // pokemonResults.forEach(result => {
-    //     console.log(result)
-    //     const pokemonDiv = document.createElement("div");
-    //     pokemonDiv.className = "col-lg pokemon-container";
-
-    //     pokemonDiv.innerHTML = `
-    //     <div class="card h-100">
-    //         <div class="text-center"><img id="pokemon-img" src="25.png"></div>
-    //         <div class="card-body">
-    //         <h5 class="card-title" id="pokemon-name">${result.name}</h5>
-    //         <div class="d-flex flex-row justify-content-center">
-    //             <div class="icon electric p-2"><img src="assets/pokemon-type-icons/electric.svg" /></div>
-    //             <div class="icon fighting p-2"><img src="assets/pokemon-type-icons/fighting.svg" /></div>
-    //         </div>
-    //         <div class="d-flex flex-row justify-content-center">
-    //             <div class="scale"><i class="bi bi-rulers"></i> 23 cm </div>
-    //             <div class="scale"><i class="bi bi-rulers"></i> 23 kg </div>
-    //         </div>
-    //         </div>
-    //         <a href="#" class="btn btn-view" style="background-color: #F2D94E;">More Details</a>
-    //     </div>
-    //     `;
-    //         pokemonContainer.appendChild(pokemonDiv);
-    // });
+function convertWeightFromHectogramsToKilograms(weightInHectograms) {
+    const weightInKilograms = (weightInHectograms / 10).toFixed(2);
+    return weightInKilograms;
 }
 
-fetchAllPokemon();
-displayPokemon();
+function convertHeightFromDecimetersToMeters(heightInDecimeters) {
+    const heightInMeters = (heightInDecimeters / 10).toFixed(2);
+    return heightInMeters;
+}  
+
+fetchAndPopulatePokemon();
